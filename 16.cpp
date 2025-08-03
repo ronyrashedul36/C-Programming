62d25c55bf65fd01eca853e20528d9c58c950f16
Md Rashedul Islam Rony <31292548+ronyrashedul36@users.noreply.github.com>
2025-08-03 21:41:54 +0600

    Basic Bank (AM) - 2019
    How many bits have to change to convert int A to int B?
    sample A  =31 and B = 14

--- 16.cpp
+++ 16.cpp
@@ -1,23 +1,30 @@
 /*
-cpp program to find sum of following series:
-1 + (1/2) + (1/3) + .. + (1/n)
+How many bits have to change to convert int A to int B?
+sample A  =31 and B = 14
 
 */
 #include<bits/stdc++.h>
 
 using namespace std;
 
+int countBitChanges(int a, int b)
+{
+
+    int xorResult = a^b;
+    int count = 0;
+    while(xorResult != 0)
+        {
+            count += xorResult & 1;
+            xorResult >>= 1;
+        }
+    return count;
+}
+
 int main()
 {
-    int n;
-    double sum = 0;
-    cout << "Enter the value of n: ";
-    cin >> n;
-    for(int i = 1; i <= n; i++)
-    {
-        sum += (1.0/i);
-    }
-    cout << "Sum of the series 1 + 1/2 + 1/3 + ... + 1/" << n << " = " << sum << endl;
+    int a,b;
+    cin >> a >> b;
+    cout << "Number of bits to change: " << countBitChanges(a, b) << endl;
 
     return 0;
 }
