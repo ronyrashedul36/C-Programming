5def258b756e15b1f13942abfc4030c8263e0f28
Md Rashedul Islam Rony <31292548+ronyrashedul36@users.noreply.github.com>
2025-11-04 16:39:56 +0600

    B. Your Name
    https://codeforces.com/contest/2167/problem/B

--- 1062.cpp
+++ 1062.cpp
@@ -4,17 +4,34 @@ using namespace std;
 
 int main()
 {
-    int t;
-    cin >> t;
-    while(t--)
+    int tt;
+    cin >> tt;
+    while(tt--)
     {
-        int a,b,c,d;
-        cin >> a >> b >> c >> d;
-        if(a==b && b==c && c==d) {
-            cout <<"YES\n";
-        } else {
-        cout <<"NO\n";
+        int n;
+        cin >> n;
+        string s,t;
+        cin >> s >> t;
+        int l = s.size();
+        map<char,int> m;
+        for(int i = 0; i < l; i++) {
+            m[s[i]]++;
+
         }
+        bool b = false;
+        for(int i = 0; i < l; i++) {
+            if(m[t[i]]){
+                m[t[i]]--;
+                continue;
+            }
+            b = true;
+            break;
+        }
+        if(b) {
+            cout <<"NO\n";
+            continue;
+        }
+        cout <<"YES\n";
     }
     return 0;
 }
